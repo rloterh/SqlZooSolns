1e9b84dc84a73c2555fdd276903e56fd1d92b6e6
Robert Loterh <rloterh@gmail.com>
2020-10-19 01:44:53 +0000

    Complete 'numeric examples' query

---
+++ 8_numeric_examples.sql
@@ -0,0 +1,62 @@
+-- 1
+SELECT A_STRONGLY_AGREE
+  FROM nss
+ WHERE question='Q01'
+   AND institution='Edinburgh Napier University'
+   AND subject='(8) Computer Science'
+
+-- 2
+SELECT institution, subject FROM nss
+WHERE question='Q15'
+AND score >= 100
+
+-- 3
+SELECT institution, score
+  FROM nss
+ WHERE question='Q15'
+   AND subject='(8) Computer Science'
+   AND score < 50
+
+-- 4
+SELECT subject, SUM(response)
+  FROM nss
+ WHERE question='Q22'
+   AND (subject='(8) Computer Science'
+    OR subject='(H) Creative Arts and Design')
+GROUP BY subject
+
+-- 5
+SELECT subject, SUM(A_STRONGLY_AGREE * response / 100)
+  FROM nss
+ WHERE question='Q22'
+   AND (subject='(8) Computer Science'
+    OR subject='(H) Creative Arts and Design')
+GROUP BY subject
+
+-- 6
+SELECT subject, ROUND(SUM(response*A_STRONGLY_AGREE)/SUM(response))
+  FROM nss
+ WHERE question='Q22'
+   AND (subject='(8) Computer Science'
+    OR subject='(H) Creative Arts and Design')
+GROUP BY subject
+
+-- 7
+SELECT institution, ROUND(SUM(score*response) / SUM(response))
+  FROM nss
+ WHERE question='Q22'
+   AND (institution LIKE '%Manchester%')
+GROUP by institution
+ORDER BY institution
+
+-- 8
+SELECT institution, sum(sample), sum(CASE
+                                      WHEN subject
+                                        LIKE '%Computer%'
+                                          THEN sample
+                                        ELSE 0
+                                      END)
+  FROM nss
+  WHERE question='Q01'
+    AND (institution LIKE '%Manchester%')
+GROUP BY institution
